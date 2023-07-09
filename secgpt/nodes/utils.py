eebb1e185f8bbff3a491de1a4395b58b9d435efe
s7ev3n <s.eleven.73@gmail.com>
2023-07-09 22:43:07 +0800

    use llama_index text2node parser

--- secgpt/nodes/utils.py
+++ secgpt/nodes/utils.py
@@ -1,10 +1,19 @@
 from typing import Dict
+from llama_index.schema import Document
+from llama_index.node_parser import SimpleNodeParser
 
+
+def to_Document(text):
+    return Document(text=text)
 
 def parse_text_into_nodes(
     text, 
     text_splitter,
     metadata: Dict = None
 ):
-    # TODO implement text spliter
-    pass
+    # FIXME implement your own parse to node
+    
+    docs = [to_Document(text)]
+    parser = SimpleNodeParser()
+
+    return parser.get_nodes_from_documents(docs)
